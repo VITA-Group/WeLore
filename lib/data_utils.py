ef541e0434e696ca33554e0c66f50abdd70e7790
Ajay Jaiswal <ajayjaiswal@utexas.edu>
2025-10-30 10:16:59 -0700

    Fix for Wikitext

--- lib/data_utils.py
+++ lib/data_utils.py
@@ -45,24 +45,15 @@ def get_c4(nsamples, seed, seqlen, tokenizer):
 # Load and process wikitext2 dataset
 def get_wikitext2(nsamples, seed, seqlen, tokenizer):
     # Load train and test datasets
-    traindata = load_dataset('wikitext', 'wikitext-2-raw-v1', split='train')
-    testdata = load_dataset('wikitext', 'wikitext-2-raw-v1', split='test')
-
+    import pandas as pd
+    # Loaded from https://huggingface.co/datasets/Salesforce/wikitext/tree/main/wikitext-2-raw-v1
+    testdata = pd.read_parquet("/root/WeLore/data/test-00000-of-00001.parquet")
+    
     # Encode datasets
-    trainenc = tokenizer(" ".join(traindata['text']), return_tensors='pt')
-    testenc = tokenizer("\n\n".join(testdata['text']), return_tensors='pt')
-
-    # Generate samples from training set
-    random.seed(seed)
-    trainloader = []
-    for _ in range(nsamples):
-        i = random.randint(0, trainenc.input_ids.shape[1] - seqlen - 1)
-        j = i + seqlen
-        inp = trainenc.input_ids[:, i:j]
-        tar = inp.clone()
-        tar[:, :-1] = -100
-        trainloader.append((inp, tar))
-    return trainloader, testenc
+    valenc = tokenizer(' '.join(testdata['text']), return_tensors='pt')
+    valenc = valenc.input_ids[:, :(256 * seqlen)]
+    valenc = TokenizerWrapper(valenc)
+    return -1, testenc
 
 
 def shuffleDict(d):
